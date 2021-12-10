00a3b55b0c9e5792b42aa7a537200b1505ef3504
dalancox <dalanjcox@gmail.com>
2021-12-09 21:35:44 -0700

    remove gitignore files

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const { engine } = require('express-handlebars');
+const connectDB = require('./config/db');
+const rateLimit = require("express-rate-limit");
 
 // Load config
 dotenv.config({ path: './config/config.env' });
+
+// connecting to DB
+
+connectDB();
 
 // Start Express
 const app = express();
@@ -21,8 +28,18 @@ app.set('view engine', '.hbs');
 //static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//limit IPs to 100
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+  });
+
+app.use(limiter);
+
 //Routes
 app.use('/', require('./routes/index'));
+app.use('/messages', require('./routes/message'));
 
 // Port 
 const PORT = process.env.PORT || 3000;
